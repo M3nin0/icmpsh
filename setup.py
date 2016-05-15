9d00a33f32aa9a6af14a0074e8ee1f1b5ec99e8a
Phelps <phelpsmemo@gmail.com>
2016-05-15 12:10:16 -0300

    Instalação unica
    Adicionando método de instalação de apenas um ficheiro. Agradeço muito a Bo Zhu por colaborar com este código e ajudar a desenvolver meu projeto. Muito Obrigado. Todos os créditos a ele.

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
+import sys
+try:
+    import py2exe
+except:
+    raw_input('Please install py2exe first...')
+    sys.exit(-1)
+
 from distutils.core import setup
-import py2exe
- 
-setup(console=['send_ip.py'])
+import shutil
+
+sys.argv.append('py2exe')
+
+setup(
+    options={
+        'py2exe': {'bundle_files': 1, 'compressed': True}
+    },
+    console=[
+        {'script': "send_ip.py"}
+    ],
+    zipfile=None,
+)
+
+shutil.move('dist\\my_program.exe', '.\\my_program.exe')
+shutil.rmtree('build')
+shutil.rmtree('dist')
